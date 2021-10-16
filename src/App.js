461f1f20933daab7b3e2c46db56a15b0e4922269
arc3us <arinjayjain380@gmail.com>
2021-10-16 22:03:01 +0530

    Hero heade initialised

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Hero from './Hero';
 import Topbar from './Topbar';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <Hero />
     </div>
   );
 }
